18eb2714c49bb856fb43d414d197417ad23ee744
Liz <lizriderwilson@gmail.com>
2020-12-12 23:43:41 -0500

    commit tree d0db240a53eaef9546b4f3fbf82ed32ce1445165

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+
+def prime?(integer)
+    if integer < 2
+        return false
+    end
+    range = (2..(integer-1)).to_a
+    range.each {|i| return false if integer % i == 0}
+    true
+end
